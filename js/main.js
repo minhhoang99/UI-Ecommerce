ba56a7c48ba3166ed34a2d0418e80029e6916ee4
NaTaShaRMN <tuananhnguyenyt@gmail.com>
2019-10-06 21:58:43 +0700

    fix instagram photo

--- js/main.js
+++ js/main.js
@@ -27,3 +27,40 @@ function hideProductInfo(index) {
 
     productInfo[index - 1].style.display = "none";
 }
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
+
+// about search
+
+var body = document.querySelector(".banner");
+body.addEventListener("click", function() {
+    var search = document.querySelector(".search");
+    search.style.display = "none";
+});
+
+var searchBtn = document.querySelector("#search-icon");
+searchBtn.addEventListener("click", function() {
+    var search = document.querySelector(".search");
+    search.style.display = "block";
+});
+
+function showSearh() {
+    var search = document.querySelector(".search");
+    if (search.style.display == "none")
+        search.style.display = "block";
+    else
+        search.style.display = "none";
+}
