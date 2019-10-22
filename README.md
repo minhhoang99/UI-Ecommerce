4c335bcb6d055b13b460a974aae9e1ebf787d4c2
NaTaShaRMN <tuananhnguyenyt@gmail.com>
2019-10-22 22:40:29 +0700

    add contact and fix js

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 ## Bài tập lớn môn Thiết kế giao diện người dùng
 ### Giảng viên : Nguyễn Văn X
 ### Nhóm X: Nguyễn Văn A, Nguyễn Văn B, Nguyễn Văn C, Nguyễn Văn D
+
 - Đề bài: Thiết kế giao diện người dùng bất kì 
+## Phân chia công việc: 
+
 Link live demo: https://natasharmn.github.io/UI-Ecommerce/index.html
--- js/main.js
+++ js/main.js
@@ -88,3 +88,34 @@ window.onload = function() {
 
     }
 }
+
+// setInterval(function(){
+//     var banner = document.querySelector(".banner");
+//     var arrImg = ["../img/banner_img.png","../img/banner1_img.png","../img/banner2_img.png"];
+//     var rand = Math.floor(Math.random()*3);
+//     banner.style.background = "url("+arr[rand]+")";
+// },3000);
+
+$(document).ready(function() {
+    function validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+    function validate() {
+        var $result = $("#result");
+        var email = $("#email").val();
+        $result.text("");
+
+        if (validateEmail(email)) {
+
+
+        } else {
+            alert(email + " is not valid :( ");
+
+        }
+        return false;
+    }
+
+    $("#validate").on("click", validate);
+});
